28f76f5cd8c0d87af4d3c4b3a4caf0ba7d594a28
caitscheverst <caitscheverst@gmail.com>
2023-12-28 22:10:41 +0000

    pushy push

--- src/ErrorPage.tsx
+++
@@ -1,8 +0,0 @@
-import { useRouteError } from "react-router-dom";
-
-export function ErrorPage() {
-      const error = useRouteError();
-      return <><h1>oh no</h1>
-        <div>went bang</div>
-      </>;
-}
